3937c2797cec1fd1f6edfdec317784e3ffb20ac0
xmrbeanx <54405749+xmrbeanx@users.noreply.github.com>
2019-08-22 15:42:14 +0100

    new website

---
+++ wp-login.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="pt-PT">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="pt-PT">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Iniciar sessão &lsaquo; godhatesacoward &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' id='dashicons-css'  href='/https://xmrbeanx.github.io/godhatesacoward/wp-includes/css/dashicons.min.css?ver=5.2.2' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='/https://xmrbeanx.github.io/godhatesacoward/wp-includes/css/buttons.min.css?ver=5.2.2' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='/https://xmrbeanx.github.io/godhatesacoward/wp-admin/css/forms.min.css?ver=5.2.2' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='/https://xmrbeanx.github.io/godhatesacoward/wp-admin/css/l10n.min.css?ver=5.2.2' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='/https://xmrbeanx.github.io/godhatesacoward/wp-admin/css/login.min.css?ver=5.2.2' type='text/css' media='all' />
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-pt-pt">
+		<div id="login">
+		<h1><a href="https://pt.wordpress.org/">Criado com WordPress</a></h1>
+	
+	<form name="loginform" id="loginform" action="/https://xmrbeanx.github.io/godhatesacoward/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Nome de utilizador ou endereço de email<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Senha<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Lembrar-me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Iniciar sessão" />
+				<input type="hidden" name="redirect_to" value="/wp-admin/" />
+					<input type="hidden" name="testcookie" value="1" />
+	</p>
+	</form>
+
+			<p id="nav">
+					<a href="/https://xmrbeanx.github.io/godhatesacoward/wp-login.php?action=lostpassword">Esqueceu-se da senha?</a>
+				</p>
+	
+	<script type="text/javascript">
+	function wp_attempt_focus(){
+	setTimeout( function(){ try{
+			d = document.getElementById('user_login');
+				d.focus();
+	d.select();
+	} catch(e){}
+	}, 200);
+	}
+
+			wp_attempt_focus();
+			if(typeof wpOnload=='function')wpOnload();
+			</script>
+
+			<p id="backtoblog"><a href="/https://xmrbeanx.github.io/godhatesacoward/">
+		&larr; Voltar a godhatesacoward	</a></p>
+			
+	</div>
+
+	
+		<script>
+	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
+	</script>
+		<div class="clear"></div>
+	</body>
+	</html>
+	
